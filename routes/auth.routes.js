e33e08bad51555fb160db5fda9837a4073f17040
Binaya Karki <Binay7587@gmail.com>
2023-02-13 17:30:50 +0545

    feat: added multer package and image upload feature added

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,8 +2,24 @@ const express = require('express');
 const app = express.Router()
 const authController = require("../app/controllers/auth.controllers");
 
+const multer = require('multer');
+const myStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${Math.random()}-${file.originalname}`);
+    },
+});
+const uploader = multer({
+    storage: myStorage,
+    dest: './public/uploads/',
+    limits: {
+        fileSize: 1000000
+    },
+});
 
-app.post('/register', authController.registerProcess);
+app.post('/register', uploader.single('image'), authController.registerProcess);
 app.post("/login", authController.loginProcess)
 
 module.exports = app;
