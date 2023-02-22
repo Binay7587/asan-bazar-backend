b021dcd1f4beeeb2987a8058ae01dded612d69d4
Binaya Karki <Binay7587@gmail.com>
2023-02-22 15:53:46 +0545

    feat: email unique validation done in register

--- app/controllers/auth.controllers.js
+++ app/controllers/auth.controllers.js
@@ -8,32 +8,37 @@ class AuthController {
     registerProcess = async (req, res, next) => {
         try {
             let payload = req.body;
-            if (req.file) {
-                payload.user_image = `/uploads/user_image/${req.file.filename}`;
+            let exists = await userService.getUserByEmail(payload.email);
+            if (exists) {
+                throw ('Email already exists.');
+            } else {
+                if (req.file) {
+                    payload.user_image = `/uploads/user_image/${req.file.filename}`;
+                }
+                //validation
+                let validatedData = await userService.validateRegisterData(payload);
+                //password encryption
+                validatedData.password = await bcrypt.hash(validatedData.password, 10);
+
+                //save to db
+                let response = await userService.registerUser(validatedData);
+
+                // Email notification
+                sendEmail({
+                    from: 'noreply@test.com',
+                    to: validatedData.email,
+                    subject: 'Account Registered!',
+                    textMessage: "Dear " + validatedData.name + ",\n\n" + 'Your account has been registered successfully.',
+                    htmlMessage: "<p>Dear " + validatedData.name + ",</p><p>Your account has been registered successfully.</p>"
+                });
+
+                res.json({
+                    result: response,
+                    msg: "Your account has been registered successfully.",
+                    status: true,
+                    meta: null
+                });
             }
-            //validation
-            let validatedData = await userService.validateRegisterData(payload);
-            //password encryption
-            validatedData.password = await bcrypt.hash(validatedData.password, 10);
-
-            //save to db
-            let response = await userService.registerUser(validatedData);
-
-            // Email notification
-            sendEmail({
-                from: 'noreply@test.com',
-                to: validatedData.email,
-                subject: 'Account Registered!',
-                textMessage: "Dear " + validatedData.name + ",\n\n" + 'Your account has been registered successfully.',
-                htmlMessage: "<p>Dear " + validatedData.name + ",</p><p>Your account has been registered successfully.</p>"
-            });
-
-            res.json({
-                result: response,
-                msg: "Your account has been registered successfully.",
-                status: true,
-                meta: null
-            });
         }
         catch (err) {
             next({ status: 400, msg: err });
