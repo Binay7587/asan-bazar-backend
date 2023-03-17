d69f7b351e2b7061677b544ce334deb9f3b22a04
Binaya Karki <Binay7587@gmail.com>
2023-03-17 12:23:21 +0545

    fix: error handling issue

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 //error handling middleware 
 app.use((error, req, res, next) => {
     let status = error.status ?? 400;
-    let msg = error.msg ?? "Something went wrong";
+    let msg = error.msg.message ? error.msg.message : error.msg ?? "Something went wrong";
     if (error instanceof MulterError) {
         if (error.code === "LIMIT_FILE_SIZE") {
             msg = "File size is too large. Max limit is 1MB";
