01d39550e46b94c122426e68eb189897b640cbdb
Binay7587 <Binay7587>
2023-05-26 14:49:53 +0545

    fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
-// app.use(cors());
+app.use(cors());
 
 const routes = require("./routes");
 const logger = require("./app/middleware/logger.middleware");
