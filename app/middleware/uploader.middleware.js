a989d51e8997a056eff81bdac2c7763e1e07d33a
Binay7587 <Binay7587>
2023-05-26 14:09:10 +0545

    fix: image upload if folder doesn't exist fixed

--- app/middleware/uploader.middleware.js
+++ app/middleware/uploader.middleware.js
@@ -1,16 +1,18 @@
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const uploadPath = path.join(process.cwd(), 'public/uploads/images');
 
 const myStorage = multer.diskStorage({
     destination: (req, file, cb) => {
-        const uploadPath = `./public/uploads/images/${file.fieldname}`;
-
-        if (!fs.existsSync(uploadPath)) {
-            fs.mkdirSync(uploadPath, { recursive: true });
+        const imagePath = `${uploadPath}/${file.fieldname}`;
+        
+        if (!fs.existsSync(imagePath)) {
+            fs.mkdirSync(imagePath, { recursive: true });
         }
 
-        cb(null, uploadPath);
+        cb(null, imagePath);
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}-${Math.random()}-${file.originalname}`);
@@ -20,18 +22,23 @@ const myStorage = multer.diskStorage({
 const imageFilter = (req, file, cb) => {
     const filetypes = /jpeg|jpg|png/;
     const mimetype = filetypes.test(file.mimetype);
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const extname = filetypes.test(
+        path.extname(file.originalname).toLowerCase()
+    );
     if (mimetype && extname) {
         return cb(null, true);
     }
-    cb({ status: 400, msg: 'File upload only supports the following filetypes - ' + filetypes });
-}
+    cb({
+        status: 400,
+        msg: "File upload only supports the following filetypes - " + filetypes,
+    });
+};
 
 const uploader = multer({
     storage: myStorage,
     fileFilter: imageFilter,
     limits: {
-        fileSize: 1000000
+        fileSize: 1000000,
     },
 });
 
